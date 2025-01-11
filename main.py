69d0bfabc918712785526ce8418d551db92c2bd1
pany0593 <1569987504@qq.com>
2025-01-11 20:21:21 +0800

    Move decrement_time from countdown.py to main.py

--- main.py
+++ main.py
@@ -2,11 +2,28 @@ import sys
 from PyQt6.QtCore import QTimer
 from PyQt6.QtWidgets import QApplication
 
-from countdown import decrement_time
 from match_singleton import MatchSingleton
 from process_message import process_message
 from websocket_manager import WebSocketManager
 from windows.MainWindow import MainWindow
+
+
+def decrement_time(window):
+    """
+    每 1/3 秒减少 Match 的倒计时时间。
+    """
+    match = MatchSingleton.get_instance()
+    time_parts = match.time.split(":")
+    if len(time_parts) == 2:
+        minutes, seconds = map(int, time_parts)
+        total_seconds = minutes * 60 + seconds
+
+        if total_seconds > 0:
+            total_seconds -= 1
+
+        minutes, seconds = divmod(total_seconds, 60)
+        match.time = f"{minutes:02}:{seconds:02}"
+        window.event_managers["bottom"].update.emit()
 
 
 # 主程序入口
