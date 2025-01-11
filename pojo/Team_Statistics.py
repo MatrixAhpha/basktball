b1fa2d78a8c78f415670ea125b9f373aa1818ce9
pany0593 <1569987504@qq.com>
2025-01-11 16:01:48 +0800

    Refactor Team_Statistics to inherit from Statistics class

--- pojo/Team_Statistics.py
+++ pojo/Team_Statistics.py
@@ -1,19 +1,16 @@
-class Team_Statistics:
+from pojo.Statistics import Statistics
+
+
+class Team_Statistics(Statistics):
     """
     技术统计类，包含队伍的比赛统计信息。
     """
 
     def __init__(self, two_points=0, three_points=0, fouls=0, blocks=0, rebounds=0):
-        self.two_points = int(two_points)  # 2分球得分
-        self.three_points = int(three_points)  # 3分球得分
-        self.fouls = int(fouls)  # 犯规数
-        self.blocks = int(blocks)  # 盖帽数
-        self.rebounds = int(rebounds)  # 篮板数
+        # 初始化父类属性
+        super().__init__(two_points, three_points, fouls, blocks, rebounds)
+        # 初始化事件管理器
         self.event_manager = None
-
-    def __str__(self):
-        return (f"2分球得分: {self.two_points}, 3分球得分: {self.three_points}, "
-                f"犯规数: {self.fouls}, 盖帽数: {self.blocks}, 篮板数: {self.rebounds}")
 
     def set_event_manager(self, event_manager):
         self.event_manager = event_manager
